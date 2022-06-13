693d6c2e8885faa19c6c08c8f35efcdf8065ea66
purity-wangu <purity.kariuki@student.moringaschool.com>
2022-06-13 13:41:16 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+function saturdayFun (activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`;
+    
+}
+saturdayFun("bathe my dog");
+
+
+const mondayWork = function(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`;
+};
+mondayWork();
+
+
+
+
+function wrapAdjective(name = "*"){
+    return function (adjective = "special"){
+        return `You are ${name}${adjective}${name}!`
+    }
+    
+}
+
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+saturdayFun();
+
+
+// const mondayWork = function(activity = "go to the office"){
+//     console.log(`This Monday, I will ${activity}.`);
+// }
+// mondayWork("work from home");
+
